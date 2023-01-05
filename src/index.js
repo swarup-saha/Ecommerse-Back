23e87eca33200c9b0536cdd5ba29add001aec91d
swarup saha <swarupsaha340@gmail.com>
2023-01-05 23:12:45 +0530

    added some changes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const app = require("./app");
 const serverless = require("serverless-http");
-
+const express = require("express");
+const router = express.Router();
+app.use("/.netlify/functions/api", router);
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
